f356b61af92f3440e1dd0b020fb3fd829f0e3d8d
ec753 <evan.cudone@yale.edu>
2023-06-27 12:26:19 -0400

    spike_centered now contains WB model

--- scripts/partial_history.py
+++ scripts/partial_history.py
@@ -2,191 +2,103 @@ import sys
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-sys.path.insert(1, "./utils/")
+sys.path.insert(1, "../utils/")
 import HH, Stimuli
 from neuron import h
 h.load_file("stdrun.hoc")
 from neuron.units import mV, ms
+import time
 
-data_dir = './data/partial_history/'
+########## HYPERPARAMETERS ##########
+data_dir = '../data/partial_history/'
 num_input_patterns_per_n = 1000
 num_histories = 1000
-ns = [n for n in range(3,31)]
+#ns = [n for n in range(3,31)]
+ns = [n for n in range(31,51)]
 
 excitatory_interval = 5
 inhibitory_interval = 15
 
+stim_types = ['base','lw','lt','lwlt','burst']
 ########## STIMULI ##########
-print('loading stimuli')
+print('generating stimuli')
+stimuli = {}
 
-with open(f'{data_dir}stimuli.json') as f:
-    stimuli = json.load(f)
+for n in ns:
+    stimuli[n] = {}
+    for pattern_ind in range(num_input_patterns_per_n):
+        stimuli[n][pattern_ind] = Stimuli.excitatory_and_inhibitory_n(excitatory_interval, inhibitory_interval, n)
+
+with open(data_dir + "stimuli_31_51.json", "w") as fout:
+    fout.write(json.dumps(stimuli))
+
 ########## HISTORIES ##########
-print('loading histories')
+print('generating histories')
+histories_sim_duration = 10000
+stim_params = Stimuli.ExperimentalStimParams()
+stim_scaffold = stim_params.stim_scaffold
 
-histories_base = np.load(f'{data_dir}histories_base.npy')
-histories_lw = np.load(f'{data_dir}histories_lw.npy')
-histories_lt = np.load(f'{data_dir}histories_lt.npy')
-histories_lwlt = np.load(f'{data_dir}histories_lwlt.npy')
-histories_burst = np.load(f'{data_dir}histories_burst.npy')
-histories_sets = {
-    'base': histories_base,
-    'lw': histories_lw,
-    'lt': histories_lt,
-    'lwlt': histories_lwlt,
-    'burst': histories_burst
-}
+histories = {}
+history_inds = np.random.randint(250, (histories_sim_duration*40)-250, num_histories)
+for stim_type in stim_types:
+    df, spikes, stims = HH.run_poisson_sim(
+        [stim_params.stim_scaffold[stim_type]['ex'], stim_params.stim_scaffold[stim_type]['in']],
+        None, histories_sim_duration)
+
+    histories[stim_type] =  np.array(
+        [
+            [df['v'][history_ind] for history_ind in history_inds],
+            [df['m'][history_ind] for history_ind in history_inds],
+            [df['h'][history_ind] for history_ind in history_inds],
+            [df['n'][history_ind] for history_ind in history_inds],
+        ]
+    )
+    np.save(f'{data_dir}histories_{stim_type}', histories[stim_type])
 
 ########## EXPERIMENT ##########
 print('starting experiment')
 
-# stim scaffold is used to dynamically generate stim objects for the simulation
-stim_scaffold = {
-    'base': {
-        'ex': Stimuli.PoissonStim(
-            'ex_base', 'ex_base',
-            interval=5,
-            rev_potential=0,
-            weight=0.0002,
-            tau=2,
-            seed='na'
-        ),
-        'in': Stimuli.PoissonStim(
-            'in_base', 'in_base',
-            interval=15,
-            rev_potential=-80,
-            weight=0.0005,
-            tau=6,
-            seed='na'
-        )
-    },
-    'lw': {
-        'ex': Stimuli.PoissonStim(
-            'ex_lw', 'ex_lw',
-            interval=5,
-            rev_potential=0,
-            weight=0.00015,
-            tau=2,
-            seed='na'
-        ),
-        'in': Stimuli.PoissonStim(
-            'in_lw', 'in_lw',
-            interval=15,
-            rev_potential=-80,
-            weight=0.0002,
-            tau=6,
-            seed='na'
-        )
-    },
-    'lt': {
-        'ex': Stimuli.PoissonStim(
-            'ex_lt', 'ex_lt',
-            interval=5,
-            rev_potential=0,
-            weight=0.0002,
-            tau=10,
-            seed='na'
-        ),
-        'in': Stimuli.PoissonStim(
-            'in_lt', 'in_lt',
-            interval=15,
-            rev_potential=-80,
-            weight=0.0005,
-            tau=40,
-            seed='na'
-        )
-    },
-    'lwlt': {
-        'ex': Stimuli.PoissonStim(
-            'ex_lwlt', 'ex_lwlt',
-            interval=5,
-            rev_potential=0,
-            weight=0.00015,
-            tau=10,
-            seed='na'
-        ),
-        'in': Stimuli.PoissonStim(
-            'in_lwlt', 'in_lwlt',
-            interval=15,
-            rev_potential=-80,
-            weight=0.0002,
-            tau=40,
-            seed='na'
-        )
-    },
-    'burst': {
-        'ex': Stimuli.PoissonStim(
-            'ex_burst', 'ex_burst',
-            interval=5,
-            rev_potential=0,
-            weight=0.0001,
-            tau=40,
-            seed='na'
-        ),
-        'in': Stimuli.PoissonStim(
-            'in_burst', 'in_burst',
-            interval=15,
-            rev_potential=-80,
-            weight=0.0005,
-            tau=20,
-            seed='na'
-        )
-    }
-}
+for n in ns:
+    for pattern_ind in range(num_input_patterns_per_n):
+        print(f'n {n}/{max(ns)}, pattern {pattern_ind}/{num_input_patterns_per_n}')
+        results = {stim_type:[] for stim_type in stim_types}
 
-print(f'running experiments with {n} inputs')
-for pattern_ind in range(num_input_patterns_per_n):
-#for pattern_ind in [50]:
-    print(f'progress for this n = {n}: {pattern_ind}/{num_input_patterns_per_n}')
+        etimes = [stim[1] for stim in stimuli[n][pattern_ind] if stim[0] == 'e']
+        itimes = [stim[1] for stim in stimuli[n][pattern_ind] if stim[0] == 'i']
 
-    start_time = time.time()
+        stim_duration = max([t for stim_type, t in stimuli[n][pattern_ind]])  # total duration of the stimuli
 
-    _stims = stimuli[str(n)][str(pattern_ind)]
-    _stims = [(stim_type, float(t)) for stim_type, t in _stims]
-    # run a bunch of cells in parallel
-    stim_duration = max([t for stim_type, t in _stims])  # total duration of the stimuli
+        cells = {}
+        fInitializeHandlers = []
 
-    _e_times = [t for stim_type, t in _stims if stim_type == 'e']
-    _i_times = [t for stim_type, t in _stims if stim_type == 'i']
+        for stim_type in stim_types:
+            stim_scaffold[stim_type]['ex'].stim_times = etimes
+            stim_scaffold[stim_type]['in'].stim_times = itimes
 
-    cells = {}
-    fInitializeHandlers = []
+            cells[stim_type] = {}
+            for history_ind in range(num_histories):
+                history = histories[stim_type][:, history_ind]
 
-    print(f'setting up {len(histories_sets) * num_input_patterns_per_n * num_histories} cells')
-    for stim_type in histories_sets:
-        cells[stim_type] = {}
-        for history_ind in range(num_histories):
-            history = histories_sets[stim_type][:, history_ind]
+                cells[stim_type][history_ind] = HH.HH()
+                cells[stim_type][history_ind].add_custom_stimulus(stim_scaffold[stim_type]['ex'])
+                cells[stim_type][history_ind].add_custom_stimulus(stim_scaffold[stim_type]['in'])
+                cells[stim_type][history_ind].sim_init(
+                    v0=history[0],
+                    m0=history[1],
+                    h0=history[2],
+                    n0=history[3]
+                )
+                fInitializeHandlers.append(h.FInitializeHandler(cells[stim_type][history_ind].do_sim_init))
 
-            cells[stim_type][history_ind] = HH.HH()
-            stim_scaffold[stim_type]['ex'].stim_times = _e_times
-            stim_scaffold[stim_type]['in'].stim_times = _i_times
-            cells[stim_type][history_ind].add_custom_stimulus(stim_scaffold[stim_type]['ex'])
-            cells[stim_type][history_ind].add_custom_stimulus(stim_scaffold[stim_type]['in'])
-            cells[stim_type][history_ind].sim_init(
-                v0=history[0],
-                m0=history[1],
-                h0=history[2],
-                n0=history[3]
-            )
-            fInitializeHandlers.append(h.FInitializeHandler(cells[stim_type][history_ind].do_sim_init))
+        h.finitialize(-65)
+        h.continuerun(stim_duration + 20 * ms)
 
-    print(f'running simulation')
-    h.finitialize(-65)
-    h.continuerun(stim_duration + 20 * ms)
-    print(f'simulation ended')
-    print(f'recording results')
-    for stim_type in histories_sets:
-        results = []
-        for history_ind in range(num_histories):
-            nsts = [spike - stim_duration for spike in list(cells[stim_type][history_ind].spike_times)]
-            nsts = [nst for nst in nsts if nst > 0]
-            if len(nsts) < 1:
-                results.append(np.nan)
-            else:
-                results.append(min(nsts))
-        np.save(f'{data_dir}results/{stim_type}_{n}_{pattern_ind}', np.array(results))
-
-    print(f'batch took {round(time.time() - start_time, 1)} sec')
-    print()
-print()
+        for stim_type in stim_types:
+            for history_ind in range(num_histories):
+                nsts = [spike - stim_duration for spike in list(cells[stim_type][history_ind].spike_times)]
+                nsts = [nst for nst in nsts if nst > 0]
+                if len(nsts) < 1:
+                    results[stim_type].append(np.nan)
+                else:
+                    results[stim_type].append(min(nsts))
+            np.save(f'{data_dir}results/{stim_type}_{n}_{pattern_ind}', np.array(results[stim_type]))
--- scripts/partial_history_WB.py
+++ scripts/partial_history_WB.py
@@ -1,45 +1,59 @@
-import sys
-import json
-import time
-import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import sys
+import json
+from scipy import stats
+import matplotlib.lines as lines
+import matplotlib as mpl
+
 sys.path.insert(1, "../utils/")
 import WB, Stimuli
+
 from neuron import h
 h.load_file("stdrun.hoc")
 from neuron.units import mV, ms
 
 ########## HYPERPARAMETERS ##########
-'''
-parser = argparse.ArgumentParser(description='Args')
-parser.add_argument('n', type=int, help='n')
-args = parser.parse_args()
-n = args.n
-'''
-
 data_dir = '../data/partial_history_WB/'
 num_input_patterns_per_n = 1000
 num_histories = 1000
-ns = [n for n in range(3,31)]
+#ns = [n for n in range(3,31)]
+ns = [n for n in range(31,51)]
 
 excitatory_interval = 5
 inhibitory_interval = 15
 
 ########## STIMULI ##########
-print('loading stimuli')
+print('generating stimuli')
+stimuli = {}
 
-with open(f'{data_dir}stimuli.json') as f:
-    stimuli = json.load(f)
+for n in ns:
+    stimuli[n] = {}
+    for pattern_ind in range(num_input_patterns_per_n):
+        stimuli[n][pattern_ind] = Stimuli.excitatory_and_inhibitory_n(excitatory_interval, inhibitory_interval, n)
+
+with open(data_dir + "stimuli.json", "w") as fout:
+    fout.write(json.dumps(stimuli))
+
 ########## HISTORIES ##########
-print('loading histories')
+print('generating histories')
+histories_sim_duration = 10000
+stim_params = Stimuli.ExperimentalStimParams()
 
-histories_wb = np.load(f'{data_dir}histories_wb.npy')
+df, spikes, stims = WB.run_poisson_sim(
+    [stim_params.stim_scaffold['wb']['ex'], stim_params.stim_scaffold['wb']['in']], histories_sim_duration)
 
-histories_sets = {
-    'wb': histories_wb
-}
+history_inds = np.random.randint(250, (histories_sim_duration*40)-250, num_histories)
+histories_wb = np.array(
+    [
+        [df['v'][history_ind] for history_ind in history_inds],
+        [df['m'][history_ind] for history_ind in history_inds],
+        [df['h'][history_ind] for history_ind in history_inds],
+    ]
+)
 
+np.save(data_dir+'histories_wb', histories_wb)
 ########## EXPERIMENT ##########
 print('starting experiment')
 
@@ -47,59 +61,43 @@ print('starting experiment')
 stim_scaffold = Stimuli.ExperimentalStimParams()
 stim_scaffold = stim_scaffold.stim_scaffold
 
+# stim scaffolds
+estims = stim_params.stim_scaffold['wb']['ex']
+istims = stim_params.stim_scaffold['wb']['in']
+
 for n in ns:
-    print(f'running experiments with {n} inputs')
     for pattern_ind in range(num_input_patterns_per_n):
-    #for pattern_ind in [50]:
-        print(f'progress for this n = {n}: {pattern_ind}/{num_input_patterns_per_n}')
+        print(f'n {n}/{max(ns)}, pattern {pattern_ind}/{num_input_patterns_per_n}')
+        results = []
+        stim_duration = max([t for stim_type, t in stimuli[n][pattern_ind]])  # total duration of the stimuli
 
-        start_time = time.time()
-
-        _stims = stimuli[str(n)][str(pattern_ind)]
-        _stims = [(stim_type, float(t)) for stim_type, t in _stims]
-        # run a bunch of cells in parallel
-        stim_duration = max([t for stim_type, t in _stims])  # total duration of the stimuli
-
-        _e_times = [t for stim_type, t in _stims if stim_type == 'e']
-        _i_times = [t for stim_type, t in _stims if stim_type == 'i']
+        estims.stim_times = [stim[1] for stim in stimuli[n][pattern_ind] if stim[0] == 'e']
+        istims.stim_times = [stim[1] for stim in stimuli[n][pattern_ind] if stim[0] == 'i']
 
         cells = {}
         fInitializeHandlers = []
 
-        print(f'setting up {len(histories_sets) * num_input_patterns_per_n * num_histories} cells')
-        for stim_type in histories_sets:
-            cells[stim_type] = {}
-            for history_ind in range(num_histories):
-                history = histories_sets[stim_type][:, history_ind]
+        for history_ind in range(num_histories):
+            history = histories_wb[:, history_ind]
 
-                cells[stim_type][history_ind] = WB.WB()
-                stim_scaffold[stim_type]['ex'].stim_times = _e_times
-                stim_scaffold[stim_type]['in'].stim_times = _i_times
-                cells[stim_type][history_ind].add_custom_stimulus(stim_scaffold[stim_type]['ex'])
-                cells[stim_type][history_ind].add_custom_stimulus(stim_scaffold[stim_type]['in'])
-                cells[stim_type][history_ind].sim_init(
-                    v0=history[0],
-                    m0=history[1],
-                    h0=history[2],
-                )
-                fInitializeHandlers.append(h.FInitializeHandler(cells[stim_type][history_ind].do_sim_init))
+            cells[history_ind] = WB.WB()
+            cells[history_ind].add_custom_stimulus(estims)
+            cells[history_ind].add_custom_stimulus(istims)
+            cells[history_ind].sim_init(v0=history[0], m0=history[1], h0=history[2])
 
-        print(f'running simulation')
-        h.finitialize(-65)
-        h.continuerun(stim_duration + 20 * ms)
-        print(f'simulation ended')
-        print(f'recording results')
-        for stim_type in histories_sets:
-            results = []
-            for history_ind in range(num_histories):
-                nsts = [spike - stim_duration for spike in list(cells[stim_type][history_ind].spike_times)]
-                nsts = [nst for nst in nsts if nst > 0]
-                if len(nsts) < 1:
-                    results.append(np.nan)
-                else:
-                    results.append(min(nsts))
-            np.save(f'{data_dir}results/{stim_type}_{n}_{pattern_ind}', np.array(results))
+            fInitializeHandlers.append(h.FInitializeHandler(cells[history_ind].do_sim_init))
 
-        print(f'batch took {round(time.time() - start_time, 1)} sec')
-        print()
-print()
+        h.finitialize()
+        h.continuerun(stim_duration + 20)
+
+        for history_ind in range(num_histories):
+            nsts = [spike - stim_duration for spike in list(cells[history_ind].spike_times)]
+            nsts = [nst for nst in nsts if nst > 0]
+            if len(nsts) < 1:
+                results.append(np.nan)
+            else:
+                results.append(min(nsts))
+
+        np.save(f'{data_dir}results/wb_{n}_{pattern_ind}', np.array(results))
+
+    print()
